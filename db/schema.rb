99bf47eeddd3dafafb2177c156d24e284fb42cd3
MishaHerscu <mishaherscu@gmail.com>
2016-06-22 10:30:36 -0400

    added migration for articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804215003) do
+ActiveRecord::Schema.define(version: 20160622142401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
